cb0b42d39b0aed9a7a446d67b798f85cf7d5d5e8
Laura Quintero <quintero.laura.a@gmail.com>
2024-05-29 23:37:01 -0500

    wip add toolltip

--- src/app/components/ListItem.tsx
+++ src/app/components/ListItem.tsx
@@ -1,20 +1,34 @@
+'use client';
+
 import React from 'react';
 import IconButton from './IconButton';
+import { Tooltip } from 'react-tooltip';
+
 
 interface ListItemProps {
+  id?: string;
   text?: string;
   callback?: () => void;
   icon?: React.ReactNode;
 }
 
-const ListItem: React.FC<ListItemProps> = ({ text, callback, icon }) => {
+const ListItem: React.FC<ListItemProps> = ({ id, text, callback, icon }) => {
     return (
-      <li className={`flex items-center p-2 border-b'}`}>
+      <li
+        id={id}
+        className={`flex items-center p-2 border-b`}
+      >
+        <div
+          data-tooltip-id="my-tooltip"
+          data-tooltip-content="Hello world!"
+        >
         {icon ? (
             IconButton({ icon, onClick: callback, label: text })
         ) : (
             <span onClick={callback}>{text}</span>
         )}
+        </div>
+        <Tooltip id="my-tooltip"/>
       </li>
     );
   };
