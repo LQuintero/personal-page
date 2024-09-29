03472398e020195d7abbbaefd875c08c6e79cd40
Laura Quintero <quintero.laura.a@gmail.com>
2024-09-28 22:54:21 -0400

    with resume button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 import { useRouter } from 'next/navigation';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedin, faSquareGithub } from '@fortawesome/free-brands-svg-icons';
-import { faSquareEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faSquareEnvelope, faFile } from '@fortawesome/free-solid-svg-icons';
 
 import Home from '../components/HomePage';
 import { ListItemProps } from '../types/ListItemProps';
@@ -19,42 +19,53 @@ const App = () => {
     router.push('/contact');
   };
 
-  const itemIconClassesShared = 'h-[36px] place-content-center';
-  const itemIconClassesBrands = `h-9 ${itemIconClassesShared}`;
-  const itemIconClassesRegular = `h-9 ${itemIconClassesShared}`;
-
+  const itemIconClassesShared = 'place-content-center';
+  const itemIconClassesShort = `h-[36px] ${itemIconClassesShared}`;
+  const itemIconClassesTall = `h-[32px] ${itemIconClassesShared}`;
+  const tooltipPosition = 'bottom';
+  
   const listItems: ListItemProps[] = [
     {
       type: 'link',
       item: {
         id: '1',
-        icon: <FontAwesomeIcon id="linkedin" icon={faLinkedin} className={itemIconClassesBrands} />,
+        icon: <FontAwesomeIcon id="linkedin" icon={faLinkedin} className={itemIconClassesShort} />,
         uri: "https://www.linkedin.com/in/quinterolaura/",
         openInNewTab: true
       },
       tooltipText: 'LinkedIn',
-      tooltipPosition: 'bottom'
+      tooltipPosition: tooltipPosition
     },
     {
       type: 'link',
       item: {
         id: '2',
-        icon: <FontAwesomeIcon id="github" icon={faSquareGithub} className={itemIconClassesBrands} />,
+        icon: <FontAwesomeIcon id="github" icon={faSquareGithub} className={itemIconClassesShort} />,
         uri: "https://github.com/LQuintero",
         openInNewTab: true
       },
       tooltipText: 'GitHub',
-      tooltipPosition: 'bottom'
+      tooltipPosition: tooltipPosition
     },
     {
       type: 'button',
       item: {
         id: '3',
-        icon: <FontAwesomeIcon id="contact" icon={faSquareEnvelope} className={itemIconClassesRegular} />,
+        icon: <FontAwesomeIcon id="contact" icon={faSquareEnvelope} className={itemIconClassesShort} />,
         onClick: handleContactButtonClick
       },
       tooltipText: 'Contact Me',
-      tooltipPosition: 'bottom'
+      tooltipPosition: tooltipPosition
+    },
+    {
+      type: 'button',
+      item: {
+        id: '4',
+        icon: <FontAwesomeIcon id="resume" icon={faFile} className={itemIconClassesTall} />,
+        onClick: handleContactButtonClick
+      },
+      tooltipText: 'Resume',
+      tooltipPosition: tooltipPosition
     }
   ];
 
