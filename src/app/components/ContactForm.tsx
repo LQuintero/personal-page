685d47947aab31d11d04bd83689f1d3604e1a406
Laura Quintero <quintero.laura.a@gmail.com>
2024-10-14 23:35:08 -0400

    Added contact form

--- src/app/components/ContactForm.tsx
+++ src/app/components/ContactForm.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
+import emailjs from 'emailjs-com';
 
 const ContactForm: React.FC = () => {
+  const [status, setStatus] = useState('');
   // State to hold form values
   const [formData, setFormData] = useState({
-    firstname: '',
-    lastname: '',
+    firstName: '',
+    lastName: '',
     email: '',
     subject: '',
     message: ''
@@ -22,34 +24,44 @@ const ContactForm: React.FC = () => {
   // Handle form submission
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    // You can process form data here (e.g., send to API)
     console.log('Form submitted:', formData);
-    alert('Message sent successfully!');
-
-    // Reset form after submission
-    setFormData({
-      firstname: '',
-      lastname: '',
-      email: '',
-      subject: '',
-      message: ''
-    });
+    setStatus('Sending...');
+    console.log("process.env.EMAIL_SERVICE_ID", process.env.NEXT_PUBLIC_EMAIL_SERVICE_ID);
+    emailjs
+      .send(process.env.NEXT_PUBLIC_EMAIL_SERVICE_ID, process.env.NEXT_PUBLIC_EMAIL_TEMPLATE_ID, formData, process.env.NEXT_PUBLIC_EMAIL_USER_ID)
+      .then(
+        (response) => {
+          console.log('Email sent successfully!', response.status, response.text);
+          setStatus('Message sent successfully!');
+          // Reset form after submission
+          setFormData({
+            firstName: '',
+            lastName: '',
+            email: '',
+            subject: '',
+            message: ''
+          });
+        },
+        (error) => {
+          console.error('Failed to send email:', error);
+          setStatus('Failed to send message. Please try again later.');
+        }
+      );
   };
 
   // Common styles for input and textarea
   const inputClasses = "mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2 text-black";
-
   const labelClasses = "block text-sm font-medium text-gray-700";
 
   return (
     <form onSubmit={handleSubmit} className="space-y-4 max-w-md mx-auto p-6 border rounded-lg shadow-lg min-w-[448px]">
       <div>
-        <label htmlFor="firstname" className={labelClasses}>First Name</label>
+        <label htmlFor="firstName" className={labelClasses}>First Name</label>
         <input
           type="text"
-          id="firstname"
-          name="firstname"
-          value={formData.firstname}
+          id="firstName"
+          name="firstName"
+          value={formData.firstName}
           onChange={handleChange}
           className={inputClasses}
           required
@@ -57,12 +69,12 @@ const ContactForm: React.FC = () => {
       </div>
 
       <div>
-        <label htmlFor="lastname" className={labelClasses}>Last Name</label>
+        <label htmlFor="lastName" className={labelClasses}>Last Name</label>
         <input
           type="text"
-          id="lastname"
-          name="lastname"
-          value={formData.lastname}
+          id="lastName"
+          name="lastName"
+          value={formData.lastName}
           onChange={handleChange}
           className={inputClasses}
           required
