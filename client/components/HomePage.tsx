e42ca9e959e60556a5f9f1a4fc9e3bf2e35d5edc
Laura Quintero <quintero.laura.a@gmail.com>
2026-08-09 15:49:03 -0400

    Refactor HomePage layout and styles for improved structure and responsiveness
    - Rename hero class to identity for better semantic clarity.
    - Adjust margin and padding values for improved spacing and alignment.
    - Update CSS properties to enhance responsiveness and visual consistency across devices.

--- client/components/HomePage.tsx
+++ client/components/HomePage.tsx
@@ -7,15 +7,15 @@ export default function Home({ title, subtitle, list }: HomePageProps) {
   return (
     <main className={`${styles.main} px-6`}>
       <div className={styles.content}>
-        <div className={styles.hero}>
+        <div className={styles.identity}>
           <h1 className="text-5xl sm:text-6xl font-bold text-center tracking-wide">
             {title}
           </h1>
-          <p className="text-xl sm:text-2xl text-center tracking-wide mt-[1rem] sm:mt-[1.575rem]">
+          <p className="text-xl sm:text-2xl text-center tracking-wide mt-1.5 sm:mt-2">
             {subtitle}
           </p>
 
-          <div id="links" className="inline-block mt-[0.95rem] sm:mt-[1.496rem]">
+          <div id="links" className="inline-block mt-2 sm:mt-2.5">
             <List items={list} isHorizontal={true} />
           </div>
         </div>
